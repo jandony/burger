0b9831a95248ed59d3f20b2933c8689d6c3612f0
Jeff Andony <jeff.andony@gmail.com>
2019-07-12 17:32:40 -0600

    uploading files for hw.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double cheeseburger', false);
